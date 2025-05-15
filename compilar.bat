5bd0035baf8afdcab9a37db07e39d257dbeac825
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2025-05-15 12:22:03 -0600

    Agregando archivos .bat de automatización

---
+++ compilar.bat
@@ -0,0 +1,30 @@
+rem Comando para ocultar la ejecucion de los siguientes comandos en la consola
+@echo off
+
+rem Comando para realizar comentarios
+
+rem Paso 1. Se definen las variables por usar
+set SRC=src\main\*.java src\system\*.java
+set SALIDA=target\classes
+set CLASSPATH=lib\flatlaf-intellij-themes-1.2.jar;lib\flatlaf-1.1.2.jar;lib\Compilador_HybriSE.jar
+set JAR=target\MiApp.jar
+set ARCHIVO_MANIFEST=META-INF/MANIFEST.MF
+
+rem Paso 2. Se limpia el directorio que almacena el archivo jar y las compilaciones de los archivos
+echo Limpiando directorio de salida...
+rmdir /s /q target
+mkdir %SALIDA%
+rem Se compilan los archivos y se almacenan en el directorio de salida
+echo Compilando los archivos y guardando en directortio de salida...
+javac -cp "%CLASSPATH%" -d %SALIDA% %SRC%
+
+rem Paso 3. Crear archivo JAR
+echo Creando archivo MANIFEST al vuelo y guardando archivo jar en directorio de salida...
+jar cfm %JAR% %ARCHIVO_MANIFEST% -C %SALIDA% .
+
+rem Paso 5. Copiando bibliotecas en el directorio de salida
+echo Copiando bibliotecas para el funcionamiento del jar...
+mkdir target\lib
+copy lib\*.jar target\lib\
+
+echo Empaquetado terminado
---
+++ ejecutar.bat
@@ -0,0 +1,3 @@
+
+
+java -jar target\MyApp.jar
